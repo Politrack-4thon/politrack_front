5606322a5cb13b89224b75abaf9377c148b0e4c5
benniejung <122111459+benniejung@users.noreply.github.com>
2023-11-09 16:42:48 +0900

    :sparkles: 프로필디테일 API 연결
    .

---
+++ src/components/Main/MainLocation.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainLocation(props) {
+    return (
+        <div style={{position:'absolute'}}>
+            <img src="src/assets/images/location.png"/>
+        </div>
+    )
+}
+
+export default MainLocation;
