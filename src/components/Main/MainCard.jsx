d85f05992a95d0d2939f1da23d2f120e354b355a
Ohchanju <ohchanju3@naver.com>
2023-11-10 08:11:59 +0900

    :lipstick: 메인 페이지 반응형 ing

--- src/components/Main/MainCard.jsx
+++ src/components/Main/MainCard.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import * as S from "./style";
+import * as S from './style';
 
 function MainCard(props) {
-    
-    return (
-        <S.MainCardContainer style={{width: '170px', height: '230px', marginLeft: '20px'}}>
-            <Link to ={`/Detail/:${props.id}`}>
-                <S.MainCardImage style={{width: '84px', height: '84px'}}>
-                    <img src='src/assets/images/default_profile.png' alt="국회의원 이미지" />
-                </S.MainCardImage>
-            </Link>
-            <S.MainCardParty style={{width: '70px', height: '24px'}}>{props.POLY_NM}</S.MainCardParty>
-            <S.MainCardName style={{fontSize:'12px'}}>{props.HG_NM}&#40;{props.ENG_NM}&#41;</S.MainCardName>
-            <S.MainCardDistrict>{props.ORIG_NM}</S.MainCardDistrict>
-            <S.MainCardUrl>
-                <a href={props.HOMEPAGE} target="_blank" rel="noopener noreferrer">
-                    <img src='\src\assets\images\card_home.svg' alt="Home Logo" />
-                </a>
-            </S.MainCardUrl>
-        </S.MainCardContainer>
-    );
+  return (
+    // <S.MainCardWhole>
+    <S.MainCardContainer>
+      <Link to={`/Detail/:${props.id}`}>
+        <S.MainCardImage style={{ width: '84px', height: '84px' }}>
+          <img
+            src='src/assets/images/default_profile.png'
+            alt='국회의원 이미지'
+          />
+        </S.MainCardImage>
+      </Link>
+      <S.MainCardParty style={{ width: '70px', height: '24px' }}>
+        {props.POLY_NM}
+      </S.MainCardParty>
+      <S.MainCardName style={{ fontSize: '12px' }}>
+        {props.HG_NM}&#40;{props.ENG_NM}&#41;
+      </S.MainCardName>
+      <S.MainCardDistrict>{props.ORIG_NM}</S.MainCardDistrict>
+      <S.MainCardUrl>
+        <a href={props.HOMEPAGE} target='_blank' rel='noopener noreferrer'>
+          <img src='\src\assets\images\card_home.svg' alt='Home Logo' />
+        </a>
+      </S.MainCardUrl>
+    </S.MainCardContainer>
+    // </S.MainCardWhole>
+  );
 }
 
 export default MainCard;
--- src/components/Main/MainSelectBtn.jsx
+++ src/components/Main/MainSelectBtn.jsx
@@ -1,30 +1,25 @@
-import React from "react";
-import * as S from "./style";
+import React from 'react';
+import * as S from './style';
 
 function MainSelectBtn(props) {
-    const names = [
-        {
-          id: 1,
-          name: '더불어민주당',
-        },
-        {
-          id: 2,
-          name: '국민의 힘',
-        },
-      ];
-    
-    return (
-        <S.ButtonContainer>
-            {names.map(data => {
-            return (
-                <S.SelectButton key={data.id}>{data.name}</S.SelectButton>
-                
-                
-            )
-            })}
-        </S.ButtonContainer>
-        
-);
+  const names = [
+    {
+      id: 1,
+      name: '더불어민주당',
+    },
+    {
+      id: 2,
+      name: '국민의 힘',
+    },
+  ];
+
+  return (
+    <S.ButtonContainer>
+      {names.map((data) => {
+        return <S.SelectButton key={data.id}>{data.name}</S.SelectButton>;
+      })}
+    </S.ButtonContainer>
+  );
 }
 
 export default MainSelectBtn;
